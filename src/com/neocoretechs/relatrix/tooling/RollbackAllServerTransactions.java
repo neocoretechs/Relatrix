0cea83ecb1d434d9957d28ae08bc7edd55dc3fa2
NeoCoreTechs - New Core Technologies <groffj@neocoretechs.com>
2023-12-21 15:30:39 -0800

    Cleanup transaction admin docs, clarify UI.

--- src/com/neocoretechs/relatrix/tooling/RollbackAllServerTransactions.java
+++ src/com/neocoretechs/relatrix/tooling/RollbackAllServerTransactions.java
@@ -1,8 +1,17 @@
 package com.neocoretechs.relatrix.tooling;
 
-import com.neocoretechs.relatrix.client.RelatrixClientTransaction;
 import com.neocoretechs.relatrix.client.RelatrixKVClientTransaction;
-
+/**
+ * Roll back all in progress transaction on the server. Use with caution.
+ * Exceptions likely to be thrown server side if active processes. use is mainly
+ * for leftover zombie transactions.<p/>
+ * According to docs states are:
+ * AWAITING_COMMIT AWAITING_PREPARE AWAITING_ROLLBACK COMMITED COMMITTED (?)
+ * LOCKS_STOLEN PREPARED ROLLEDBACK STARTED. In practice, it seems to mainly vary between
+ * STARTED and COMMITTED. The odd 'COMMITED' state doesnt seem to manifest.
+ * @author Jonathan Groff (C) Copyright NeoCoreTechs 2023
+ *
+ */
 public class RollbackAllServerTransactions {
 	public static void main(String[] args) throws Exception {
 		RelatrixKVClientTransaction rkvc = new RelatrixKVClientTransaction(args[0], args[1], Integer.parseInt(args[2]));
--- src/com/neocoretechs/relatrix/tooling/RollbackServerTransaction.java
+++ src/com/neocoretechs/relatrix/tooling/RollbackServerTransaction.java
@@ -1,7 +1,17 @@
 package com.neocoretechs.relatrix.tooling;
 
 import com.neocoretechs.relatrix.client.RelatrixKVClientTransaction;
-
+/**
+ * Roll back selected transactiosn on the server. Use with caution.
+ * Exceptions likely to be thrown server side if active processes. use is mainly
+ * for leftover zombie transactions.<p/>
+ * According to docs states are:
+ * AWAITING_COMMIT AWAITING_PREPARE AWAITING_ROLLBACK COMMITED COMMITTED (?)
+ * LOCKS_STOLEN PREPARED ROLLEDBACK STARTED. In practice, it seems to mainly vary between
+ * STARTED and COMMITTED. The odd 'COMMITED' state doesnt seem to manifest.
+ * @author Jonathan Groff (C) Copyright NeoCoreTechs 2023
+ *
+ */
 public class RollbackServerTransaction {
 	public static void main(String[] args) throws Exception {
 		RelatrixKVClientTransaction rkvc = new RelatrixKVClientTransaction(args[0], args[1], Integer.parseInt(args[2]));
@@ -18,7 +28,7 @@ public class RollbackServerTransaction {
 		if(i == 1)
 			System.out.println("Enter '1' to confirm");
 		else
-			System.out.println("Select transaction to rollback 1.-"+i+".)");
+			System.out.println("Select transaction to rollback 1.-"+i+".) then hit return, or <ctrl>-C to terminate.");
 		int j = 0;
 		StringBuilder asc = new StringBuilder(0);
 		while(j != 10) {
