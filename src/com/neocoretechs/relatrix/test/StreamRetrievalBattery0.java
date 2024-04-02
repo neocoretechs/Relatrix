74aeac34be71f41fd2bcd030d49884597ce5ead5
NeoCoreTechs - New Core Technologies <groffj@neocoretechs.com>
2024-04-02 16:22:54 -0700

    Stream retrieval test

--- src/com/neocoretechs/relatrix/test/StreamRetrievalBattery0.java
+++ src/com/neocoretechs/relatrix/test/StreamRetrievalBattery0.java
@@ -12,9 +12,15 @@ import com.neocoretechs.relatrix.client.RelatrixClient;
  * @author Jonathan Groff (c) 2021
  *
  */
-public class StreamRetrievalBattery {
+public class StreamRetrievalBattery0 {
 	public static boolean DEBUG = false;
 	private static RelatrixClient rkvc;
+	public static int displayLinesOn[]= {0,1000,5000,9990,15000,20000,30000,40000,50000,60000,70000,80000,90000,99000};
+	public static int displayLinesOff[]= {100,1100,5100,9999,15999,20999,30999,40999,50999,60999,70999,80999,90999,100000};
+	public static int displayLine = 0;
+	public static int displayLineCtr = 0;
+	public static long displayTimer = 0;
+	private static boolean DISPLAY = false;
 
 	/**
 	*/
@@ -24,12 +30,28 @@ public class StreamRetrievalBattery {
 		System.out.println("TEST BATTERY COMPLETE.");	
 		System.exit(1);
 	}
+	public static void displayCtrl() {
+		if(displayLine == 0)
+			displayLineCtr = 0;
+		if(displayLine >= displayLinesOn[displayLineCtr] && displayLine <= displayLinesOff[displayLineCtr]) {
+			if(!DISPLAY)
+				displayTimer = System.currentTimeMillis();
+			DISPLAY  = true;
+		} else {
+			if(DISPLAY)
+				System.out.println("Time between lines:"+displayLinesOn[displayLineCtr]+" and "+displayLinesOff[displayLineCtr]+" is "+(System.currentTimeMillis()-displayTimer)+" ms.");
+			DISPLAY = false;
+			if(displayLine > displayLinesOff[displayLineCtr] && displayLineCtr < displayLinesOff.length-1)
+				++displayLineCtr;
+		}
+		++displayLine;
+	}
 	/**
 	 * @param argv
 	 * @throws Exception
 	 */
 	public static void battery1(String[] argv) throws Exception {
-		System.out.println("Stream Battery1 ");
+		System.out.println("Stream Retrieval Battery0 ");
 		String fmap;
 		long tims = System.currentTimeMillis();
 		int recs = 0;
@@ -42,50 +64,76 @@ public class StreamRetrievalBattery {
 	
 		Stream it = null;
 		System.out.println("Wildcard queries:");
-	
-		System.out.println("1.) FindStream(*,*,*)...");
-		rkvc.findStream("*", "*", "*").forEach(System.out::println);
 		
-		System.out.println("2.) FindStream(*,*,?)...");		
-		rkvc.findStream("*", "*", "?").forEach(e -> {
-			if(ar.size() == 0 ) ar.add( (Result) e);	
-			System.out.println(e);
+		displayLine = 0;
+		System.out.println("1.) FindStream(*,*,*)...");
+		rkvc.findStream("*", "*", "*").forEach(e -> {//(System.out::println);
+			displayCtrl();
+			if(DISPLAY)
+				System.out.println(displayLine+"="+e);
 		});
 		
+		displayLine = 0;
+		System.out.println("2.) FindStream(*,*,?)...");		
+		rkvc.findStream("*", "*", "?").forEach(e -> {
+			if(ar.size() == 0 ) ar.add( (Result) e);
+			displayCtrl();
+			if(DISPLAY)
+				System.out.println(displayLine+"="+e);
+		});
+		
+		displayLine = 0;		
 		System.out.println("3.) FindStream(*,?,*)...");		
 		rkvc.findStream("*", "?", "*").forEach(e -> {
 			if(ar.size() == 1 ) ar.add((Result) e);
-			System.out.println(e);
+			displayCtrl();
+			if(DISPLAY)
+				System.out.println(displayLine+"="+e);
 		});
 		
+		displayLine = 0;
 		System.out.println("4.) FindStream(?,*,*)...");		
 		rkvc.findStream("?", "*", "*").forEach(e -> {
 			if(ar.size() == 2 ) ar.add( (Result) e);
-			System.out.println(e);
+			displayCtrl();
+			if(DISPLAY)
+				System.out.println(displayLine+"="+e);
 		});
 		
+		displayLine = 0;
 		System.out.println("5.) FindStream(*,?,?)...");		
 		rkvc.findStream("*", "?", "?").forEach(e -> {
 			if(ar2.size() == 0) ar2.add( (Result) e);
-			System.out.println(e);
+			displayCtrl();
+			if(DISPLAY)
+				System.out.println(displayLine+"="+e);
 		});
 		
+		displayLine = 0;
 		System.out.println("6.) FindStream(?,*,?)...");		
 		rkvc.findStream("?", "*", "?").forEach(e -> {
 			if(ar2.size() == 1) ar2.add((Result) e);
-			System.out.println(e);
+			displayCtrl();
+			if(DISPLAY)
+				System.out.println(displayLine+"="+e);
 		});
 		
+		displayLine = 0;
 		System.out.println("7.) FindStream(?,?,*)...");		
 		rkvc.findStream("?", "?", "*").forEach(e -> {
 			if(ar2.size() == 2) ar2.add((Result) e);
-			System.out.println(e);
+			displayCtrl();
+			if(DISPLAY)
+				System.out.println(displayLine+"="+e);
 		});
 		
+		displayLine = 0;
 		System.out.println("8.) FindStream(?,?,?)...");		
 		rkvc.findStream("?", "?", "?").forEach(e -> {
 			if(ar3.size() == 0) ar3.add((Result) e);
-			System.out.println(e);
+			displayCtrl();
+			if(DISPLAY)
+				System.out.println(displayLine+"="+e);
 		});
 		
 		System.out.println("Above are all the wildcard permutations. Now retrieve those with object references using the");
@@ -124,7 +172,7 @@ public class StreamRetrievalBattery {
 		System.out.println("21.) FindStream(<obj>,<obj>,?)...");		
 		rkvc.findStream(ar2.get(2).get(0), ar2.get(2).get(1), "?").forEach(e -> System.out.println(e.getClass().getName()+","+e));
 		
-		System.out.println("BATTERY1 SUCCESS in "+(System.currentTimeMillis()-tims));
+		System.out.println("StreamRetrievalBattery0 SUCCESS in "+(System.currentTimeMillis()-tims));
 	}
 	
 
