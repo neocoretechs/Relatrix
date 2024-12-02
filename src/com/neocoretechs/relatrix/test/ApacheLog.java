c9685322008ca19d77a5f4cfdbf35bf56b34c676
NeoCoreTechs - New Core Technologies <groffj@neocoretechs.com>
2024-12-01 19:03:25 -0800

    Added simplified log format processing to apache log file parse and store test

--- src/com/neocoretechs/relatrix/test/ApacheLog.java
+++ src/com/neocoretechs/relatrix/test/ApacheLog.java
@@ -28,15 +28,16 @@ import com.neocoretechs.relatrix.Relatrix;
 import com.neocoretechs.relatrix.Result;
 
 /**
- * Process the apache log files and place in a Relatrix database.
+ * Process the apache log files and place in a Relatrix database.<p/>
  * Store the relationship (accessLogEntryEpoch,"accessed by",remoteHost) and use this relationship
  * as the domain of the remainder of the log entry data.<p/>
  * Comparable rel = Relatrix.store(accessLogEntryEpoch,"accessed by",remoteHost);<br/>
- * Relatrix.store(rel, "remote user", remoteUser); // unreliable info field remoteUser<br/>
- * Relatrix.transactionalStore(rel, "access time",accessLogEntryEpoch);<br/>
+ * Relatrix.store(rel, "access time",accessLogEntryEpoch);<br/>
  * Relatrix.store(rel, "client request",clientRequest);<br/>
  * Relatrix.store(rel, "http status",httpStatusCode);<br/>
  * Relatrix.store(rel, "bytes returned",numBytes);<br/>
+ * If we are using the more complex format, store additional relationships:<br/>
+ * Relatrix.store(rel, "remote user", remoteUser); // unreliable info field remoteUser<br/>
  * Relatrix.store(rel, "referer",referer);<br/>
  * Relatrix.store(rel, "user agent",userAgent);<br/>
  * Relatrix.store(rel, "OS",Os);<br/>
@@ -52,7 +53,8 @@ import com.neocoretechs.relatrix.Result;
  *			} <br/>
  *			it2.forEachRemaining(System.out::println);<br/>
  *		});<br/>
- * This allows us to select sets of primary relationships, then subsets of related data.
+ * This allows us to select sets of primary relationships, then subsets of related data. Our
+ * retrieval is robust despite differing log formats, thus showing the power of the relationship structure.
  * @author Jonathan Groff Copyright (C) NeoCoreTechs 2020,2024
  *
  */
@@ -80,10 +82,13 @@ public class ApacheLog {
 
 	SimpleDateFormat accesslogDateFormat = new SimpleDateFormat(dateForm);
 	Pattern accessLogPattern = Pattern.compile(getAccessLogRegex(),Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+	Pattern accessLogPattern2 = Pattern.compile(getAccessLogRegex2(),Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 	Matcher accessLogEntryMatcher;
 	/**
-	* Structures Apache combined access log
-	* @return regex string
+	* 203.106.155.51 www.neocoretechs.com - [21/Jul/2013:01:18:11 -0400] 
+	* "GET /favicon.ico HTTP/1.1" 
+	* 200 894 "http://lizahanum.blogspot.com/2011/02/kebab-daging.html"
+	* "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.6 (KHTML, like Gecko) Chrome/16.0.899.0 Safari/535.6" "-";
 	*/
 	private String getAccessLogRegex() {
 		String clientHost = "^([\\d.]+)"; // Client IP, remote host
@@ -98,14 +103,200 @@ public class ApacheLog {
 		return clientHost+shouldBDash+clientRequest+requestTime+requestMethodUrl+httpStatusCode+numOfBytes+referer+agent;
 	}
 	/**
+	 * 199.72.81.55 - - [01/Jul/1995:00:00:01 -0400] "GET /history/apollo/ HTTP/1.0" 200 6245
+	 */
+	private String getAccessLogRegex2() {
+		String clientHost = "^([\\S]+)"; // Client 
+		String shouldBDash = " (\\S+)"; // -
+		String shouldBDash2 = " (\\S+)"; // -
+		String requestTime = " \\[([\\w:/]+\\s[+\\-]\\d{4})\\]"; // Date
+		String requestMethodUrl = " \"(.+?)\""; // request method and url
+		String httpStatusCode = " (\\d{3})"; // HTTP code
+		String numOfBytes = " (\\d+|(.+?))"; // Number of bytes
+		return clientHost+shouldBDash+shouldBDash2+requestTime+requestMethodUrl+httpStatusCode+numOfBytes;
+	}
+	/**
+	 * Use the regex patterns to parse out specific fields in the data
+	 * @param line
+	 * @throws ParseException
+	 */
+	public void readAndProcess(String line) throws ParseException {
+		//int index = 0;
+		if(DEBUG)
+			System.out.println("Line "+totalRecords+"="+line);
+		
+		accessLogEntryMatcher = accessLogPattern.matcher(line);
+		
+		if(!accessLogEntryMatcher.matches() && !accessLogEntryMatcher.lookingAt()) {
+			System.out.println("Can't parse:"+line );
+				throw new ParseException(line +" : couldn't be parsed", 0);
+		} else {
+			if(!accessLogEntryMatcher.matches())
+				System.out.println("Only some can be parsed!!");
+			try {
+				remoteHost = accessLogEntryMatcher.group(1);
+				if(DEBUG)
+					System.out.println("Got host:"+remoteHost);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get host:");
+			}
+			try {
+				remoteUser = accessLogEntryMatcher.group(2); // unreliable info in this field
+				if(DEBUG)
+					System.out.println("Got user:"+remoteUser);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get remote user!:");
+			}
+			try {
+				requestTime= accessLogEntryMatcher.group(4);
+				if(DEBUG)
+					System.out.println("Got request time:"+requestTime);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get request time!:");
+			}
+			try {
+				accessLogEntryEpoch = (accesslogDateFormat.parse(requestTime)).getTime();
+				if(DEBUG)
+					System.out.println("Got epoch:"+accessLogEntryEpoch);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get epoch!:");
+			}
+			try {
+				clientRequest = (String)accessLogEntryMatcher.group(5);
+				if(DEBUG)
+					System.out.println("Got client:"+clientRequest);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get client!:");
+			}
+			try {
+				httpStatusCode = Integer.parseInt(accessLogEntryMatcher.group(6));
+				if(DEBUG)
+					System.out.println("Got http stat:"+httpStatusCode);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get http status!:");
+			}
+			try {
+				numBytes = Integer.parseInt(accessLogEntryMatcher.group(7));
+				if(DEBUG)
+					System.out.println("Got numBytes:"+numBytes);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get number bytes!:");
+			}
+			try {
+				referer = accessLogEntryMatcher.group(9);
+				if(DEBUG)
+					System.out.println("Got referer:"+referer);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get referer!:");
+			}
+			try {
+				String userAgents[] = accessLogEntryMatcher.group(11).split(" ");
+				if(DEBUG)
+					System.out.println("Got userAgents:"+Arrays.toString(userAgents));
+				userAgent = userAgents[0];
+				if( userAgents.length > 1)
+					Os = userAgents[1].replace("(".subSequence(0,1), "".subSequence(0, 0));
+				else
+					Os = "N/A";
+				if( userAgents.length > 2)
+					OsVer = userAgents[2];
+				else
+					OsVer = "N/A";
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get user agents!:");
+			}
+
+			++totalRecords;
+			System.out.println(totalRecords+".) "+toString());
+			//	System.out.println("" + index + " : " +(remoteUser.split(" "))[1]);
+			//	for(index = 0; index < accessLogEntryMatcher.groupCount(); index++) {
+			//	System.out.println("Line num : " + index + " " +
+			//			accessLogEntryMatcher.group(index) );
+			//	}
+		}
+
+	}
+	/**
+	 * Use the regex patterns to parse out specific fields in the data
+	 * @param line
+	 * @throws ParseException
+	 */
+	public void readAndProcess2(String line) throws ParseException {
+		//int index = 0;
+		if(DEBUG)
+			System.out.println("Line "+totalRecords+"="+line);
+		
+		accessLogEntryMatcher = accessLogPattern2.matcher(line);
+		
+		if(!accessLogEntryMatcher.matches() && !accessLogEntryMatcher.lookingAt()) {
+			System.out.println("Can't parse:"+line );
+				throw new ParseException(line +" : couldn't be parsed", 0);
+		} else {
+			if(!accessLogEntryMatcher.matches())
+				System.out.println("Only some can be parsed!!");
+			try {
+				remoteHost = accessLogEntryMatcher.group(1);
+				if(DEBUG)
+					System.out.println("Got host:"+remoteHost);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get host:");
+			}
+			try {
+				requestTime= accessLogEntryMatcher.group(4);
+				if(DEBUG)
+					System.out.println("Got request time:"+requestTime);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get request time!:");
+			}
+			try {
+				accessLogEntryEpoch = (accesslogDateFormat.parse(requestTime)).getTime();
+				if(DEBUG)
+					System.out.println("Got epoch:"+accessLogEntryEpoch);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get epoch!:");
+			}
+			try {
+				clientRequest = (String)accessLogEntryMatcher.group(5);
+				if(DEBUG)
+					System.out.println("Got client:"+clientRequest);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get client!:");
+			}
+			try {
+				httpStatusCode = Integer.parseInt(accessLogEntryMatcher.group(6));
+				if(DEBUG)
+					System.out.println("Got http stat:"+httpStatusCode);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get http status!:");
+			}
+			try {
+				numBytes = Integer.parseInt(accessLogEntryMatcher.group(7));
+				if(DEBUG)
+					System.out.println("Got numBytes:"+numBytes);
+			} catch(IllegalStateException e) {
+				System.out.println("Cant get number bytes!:");
+			}
+	
+			++totalRecords;
+			System.out.println(totalRecords+".) "+toString());
+			//	System.out.println("" + index + " : " +(remoteUser.split(" "))[1]);
+			//	for(index = 0; index < accessLogEntryMatcher.groupCount(); index++) {
+			//	System.out.println("Line num : " + index + " " +
+			//			accessLogEntryMatcher.group(index) );
+			//	}
+		}
+
+	}
+	/**
 	 * Process all files in the given directory
-	 * @param dir
+	 * @param dir directory with log files
+	 * @param simplified true if using simplified log format
 	 * @throws IOException
 	 * @throws ParseException
 	 * @throws IllegalAccessException
 	 * @throws ClassNotFoundException
 	 */
-	public void getFiles(String dir) throws IOException, ParseException, IllegalAccessException, ClassNotFoundException {
+	public void getFiles(String dir, boolean simplified) throws IOException, ParseException, IllegalAccessException, ClassNotFoundException {
 		Path path = FileSystems.getDefault().getPath(dir);
 		DirectoryStream<Path> files = Files.newDirectoryStream(path); 
 		Iterator<Path> it = files.iterator();
@@ -130,7 +321,10 @@ public class ApacheLog {
 			}
 			System.out.println(" payload: "+load.length);
 			try {
-				processPayload(load);
+				if(simplified)
+					processPayload2(load);
+				else
+					processPayload(load);
 			} catch (DuplicateKeyException e) {
 				System.out.println("Attempt to store duplicate domain/map key "+e);
 				e.printStackTrace();
@@ -188,6 +382,27 @@ public class ApacheLog {
 		br.close();
 	}
 	/**
+	 * Parse and store a line of simplified log data
+	 * @param pl
+	 * @throws IOException
+	 * @throws ParseException
+	 * @throws DuplicateKeyException
+	 * @throws ClassNotFoundException
+	 */
+	public void processPayload2(byte[] pl) throws IOException, ParseException, DuplicateKeyException, ClassNotFoundException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(new DataInputStream(new ByteArrayInputStream(pl))));
+		String line = "";
+		while((line = br.readLine()) != null) {
+			try {
+				readAndProcess2(line);
+				storeRelatrix2();
+			} catch(ParseException | IllegalAccessException e) {
+				e.printStackTrace();
+			}
+		}
+		br.close();
+	}
+	/**
 	 * Store the parsed data as a relationship, then as a series of relationships that contain
 	 * the primary relationship as the domain of the subsequent relationships. This allows us to select sets
 	 * of primary relationships, then subsets of related data.
@@ -210,73 +425,34 @@ public class ApacheLog {
 			Relatrix.store(rel,"OS Ver.",OsVer);
 		} catch (DuplicateKeyException e) {}
 	}
+	
 	/**
-	 * Use the regex patterns to parse out specific fields in the data
-	 * @param line
-	 * @throws ParseException
+	 * Store the simplified parsed data as a relationship, then as a series of relationships that contain
+	 * the primary relationship as the domain of the subsequent relationships. This allows us to select sets
+	 * of primary relationships, then subsets of related data.
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 * @throws IOException
 	 */
-	public void readAndProcess(String line) throws ParseException {
-		//int index = 0;
-
-		accessLogEntryMatcher = accessLogPattern.matcher(line);
-		if(!accessLogEntryMatcher.matches()) {
-			System.out.println("Can't parse:"+line );
-			throw new ParseException(line +" : couldn't be parsed", 0);
-		} else {
-			remoteHost = accessLogEntryMatcher.group(1);
-			if(DEBUG)
-				System.out.println("Got host:"+remoteHost);
-			remoteUser = accessLogEntryMatcher.group(2); // unreliable info in this field
-			if(DEBUG)
-				System.out.println("Got user:"+remoteUser);
-			requestTime= accessLogEntryMatcher.group(4);
-			if(DEBUG)
-				System.out.println("Got request time:"+requestTime);
-			accessLogEntryEpoch = (accesslogDateFormat.parse(requestTime)).getTime();
-			if(DEBUG)
-				System.out.println("Got epoch:"+accessLogEntryEpoch);
-			clientRequest = (String)accessLogEntryMatcher.group(5);
-			if(DEBUG)
-				System.out.println("Got client:"+clientRequest);
-			httpStatusCode = Integer.parseInt(accessLogEntryMatcher.group(6));
-			if(DEBUG)
-				System.out.println("Got http stat:"+httpStatusCode);
-			numBytes = Integer.parseInt(accessLogEntryMatcher.group(7));
-			if(DEBUG)
-				System.out.println("Got numBytes:"+numBytes);
-			referer = accessLogEntryMatcher.group(9);
-			if(DEBUG)
-				System.out.println("Got referer:"+referer);
-			String userAgents[] = accessLogEntryMatcher.group(11).split(" ");
-			if(DEBUG)
-				System.out.println("Got userAgents:"+Arrays.toString(userAgents));
-			userAgent = userAgents[0];
-			if( userAgents.length > 1)
-				Os = userAgents[1].replace("(".subSequence(0,1), "".subSequence(0, 0));
-			else
-				Os = "N/A";
-			if( userAgents.length > 2)
-				OsVer = userAgents[2];
-			else
-				OsVer = "N/A";
-			++totalRecords;
-			System.out.println(totalRecords+".) "+toString());
-			//	System.out.println("" + index + " : " +(remoteUser.split(" "))[1]);
-			//	for(index = 0; index < accessLogEntryMatcher.groupCount(); index++) {
-			//	System.out.println("Line num : " + index + " " +
-			//			accessLogEntryMatcher.group(index) );
-			//	}
-		}
-
+	private static void storeRelatrix2() throws IllegalAccessException, ClassNotFoundException, IOException {
+		try {
+			Comparable<?> rel = Relatrix.store(accessLogEntryEpoch,"accessed by",remoteHost);
+			Relatrix.store(rel, "access time",accessLogEntryEpoch);
+			Relatrix.store(rel, "client request",clientRequest);
+			Relatrix.store(rel, "http status",httpStatusCode);
+			Relatrix.store(rel, "bytes returned",numBytes);
+		} catch (DuplicateKeyException e) {}
 	}
+
 	public String toString() {
 		return "Remote host:"+remoteHost+" Remote user:"+remoteUser+" Request time:"+accessLogEntryEpoch+" Client Request:"+clientRequest+
 				" Status:"+httpStatusCode+" Referer:"+referer+" User Agent:"+userAgent+" Os:"+Os+" ver:"+OsVer;
 	}
 	
 	/**
-	 * usage java com.neocoretechs.relatrix.test.ApacheLog <tablespace dir> [log file dir]
+	 * usage java com.neocoretechs.relatrix.test.ApacheLog <tablespace dir> [log file dir] [true]
 	 * Option is to use a directory of log files or a sample test set of canned hardcoded lines. Omit directory to use test lines.
+	 * argument 3 indicates use expanded, more complex log file format similar to synthetic test data
 	 * @param args
 	 * @throws ParseException
 	 * @throws IOException
@@ -297,11 +473,14 @@ public class ApacheLog {
 		lin3 += "\"GET /flabicon.ico HTTP/1.1\" 200 894 \"http://lizahanum.blogspot.com/2011/02/falafel.html\" ";
 		lin3 += "\"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.6 (KHTML, like Gecko) Chrome/16.0.899.0 Safari/535.6\" \"-\"";
 		
-		// get either the test line or a directory of log files
+		// get either the test line or a directory of log files, assume simple log format unless we have extra cmdl arg
 		ApacheLog alfoo = new ApacheLog();
-		if(args.length == 2) {
+		if(args.length == 2 || args.length == 3) {
 			DatabaseManager.setTableSpaceDir(args[0]);
-			alfoo.getFiles(args[1]);
+			if(args.length == 3)
+				alfoo.getFiles(args[1], false);
+			else
+				alfoo.getFiles(args[1], true);
 		} else {
 			if(args.length == 1) {
 				DatabaseManager.setTableSpaceDir(args[0]);
