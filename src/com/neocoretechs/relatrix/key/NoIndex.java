613854347c5ee0e9c55389e7db429e175b306a46
NeoCoreTechs - New Core Technologies <groffj@neocoretechs.com>
2025-07-08 21:45:44 -0700

    Update NoIndex.java

--- src/com/neocoretechs/relatrix/key/NoIndex.java
+++ src/com/neocoretechs/relatrix/key/NoIndex.java
@@ -15,7 +15,7 @@ import java.util.UUID;
  */
 public class NoIndex implements Comparable, Serializable {
 	private static final long serialVersionUID = 1L;
-	private transient DBKey dbKey;
+	private DBKey dbKey;
 	private Object instance;
 	
 	public NoIndex() {}
